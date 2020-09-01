9360dca370a241923592b42b7b9863ad2b67d0b9
mike56k <miha.is@mail.ru>
2020-09-01 03:44:57 +0300

    Updates

---
+++ precache-manifest.6749e313d60bfa5b558ef1e1de1c6f98.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87536b3a55b8035680099df116d64f90",
+    "url": "/mike56k/index.html"
+  },
+  {
+    "revision": "e13d2b0b6047a9d0ee9f",
+    "url": "/mike56k/static/css/main.b8a3f89f.chunk.css"
+  },
+  {
+    "revision": "c296ba035954a19952f2",
+    "url": "/mike56k/static/js/2.17e538c8.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/mike56k/static/js/2.17e538c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e13d2b0b6047a9d0ee9f",
+    "url": "/mike56k/static/js/main.3bf4ed92.chunk.js"
+  },
+  {
+    "revision": "bd11b202b164fec45e79",
+    "url": "/mike56k/static/js/runtime-main.c8f1080f.js"
+  }
+]);
